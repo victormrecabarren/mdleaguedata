34221c38216b484869ddede7785b997c45ec4b0e
Ryan Deist <ryandeist@gmail.com>
2025-07-16 11:18:45 -0400

    scaffolded /seed-trends route and component.

---
+++ src/components/SeedTrends.jsx
@@ -0,0 +1,11 @@
+const SeedTrends = () => {
+    return (
+        <div className="seed-trends">
+            <h1>Seed Trends</h1>
+            <p>Data on seed trends will be displayed here.</p>
+            {/* Additional content can be added here */}
+        </div>
+    );
+}
+
+export default SeedTrends;
