607fcc3583857ce8501a12b8ff0e9804e27fc2df
Ryan Deist <ryandeist@gmail.com>
2025-08-11 17:34:59 -0400

    moved compileWeeklySeeds to App.jsx level. Removed some unused HTML.

--- src/components/SeedTrends.jsx
+++ src/components/SeedTrends.jsx
@@ -1,55 +1,7 @@
-import { useEffect, useState, useRef } from "react";
-import { leagueEndpoint } from "../config";
-import { matchupsEndpoint } from "../config";
-import { compileWeeklySeeds } from "../utils/utils";
-
 const SeedTrends = ({ owners }) => {
-    const [updatedOwners, setUpdatedOwners] = useState([]);
-    const hasProcessed = useRef(false);
 
-    useEffect(() => {
-        if (!owners || owners.length === 0 || hasProcessed.current) {
-            return;
-        }
-
-        let numOfRegularSeasonGames = 0;
-        let allMatchups = [];
-
-        const fetchMatchUpData = async () => {
-            const leagueRes = await fetch(leagueEndpoint);
-            const leagueData = await leagueRes.json();
-            numOfRegularSeasonGames = leagueData.settings.last_report;
-
-            for (let i = 1; i <= numOfRegularSeasonGames; i++) {
-                const matchupRes = await fetch(`${matchupsEndpoint}/${i}`);
-                const matchupData = await matchupRes.json();
-                allMatchups.push(matchupData);
-            };
-
-            return allMatchups;
-        };
-
-        const compileSeeds = async () => {
-            allMatchups = await fetchMatchUpData();
-            
-            const freshOwners = owners.map(owner => ({
-                ...owner,
-                winCount: 0,
-                pointsCount: 0,
-                seeds: []
-            }));
-            
-            const processedOwners = compileWeeklySeeds(freshOwners, allMatchups);
-            setUpdatedOwners(processedOwners);
-            hasProcessed.current = true;
-        };
-
-        compileSeeds();
-
-    }, [owners]);
-
-    if (updatedOwners.length > 0) {
-        console.log('Owners w/ seed trends data:', updatedOwners);
+    if (owners.length > 0) {
+        console.log('Owners w/ seed trends data:', owners);
     };
 
     return (
