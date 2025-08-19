0cc607f790659853d3cdfdbc41feec3540b706fd
Victor Recabarren <vrecabarren@hubspot.com>
2025-08-19 12:40:54 -0400

    final configs

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'build',
-    },
-    base: '/mdleaguedata/',
-    server: {
-        port: 3000,
-        open: true
-    }
+  plugins: [react()],
+  build: {
+    outDir: "build",
+  },
+  base: "/mdleaguedata/",
+  server: {
+    port: 3000,
+    open: true,
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    globals: true,
+    css: true,
+  },
 });
